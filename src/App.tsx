6e53164ef638995e656171791a1e1cfe98809c58
Pikadorius <karpukea@yandex.by>
2022-09-26 22:46:51 +0300

    add some components

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,14 @@ function App() {
   return (
     <div className="App">
         <Header />
-     Hello, samurai! Let's go! Everything will be FINE! START FLYING!
-        <ul>
-            <li>Stand your ground</li>
-            <li>Step by step</li>
-            <li>Never give UP</li>
-        </ul>
+        <Welcome />
+        <Recomendations />
+        <Technologies />
     </div>
   );
 }
 
-
-function Header() {
+const Header = () => {
     return (
         <div>
             <ul className="page-header">
@@ -24,6 +20,38 @@ function Header() {
                 <li><a href="#">News</a></li>
                 <li><a href="#">Current</a></li>
             </ul>
+        </div>
+    );
+}
+
+const Recomendations = () => {
+    return (
+        <div>
+            <p className="important">You CAN do it, just follow these directions:</p>
+            <ul>
+                <li>Stand your ground</li>
+                <li>Step by step</li>
+                <li>Never give UP</li>
+            </ul>
+        </div>
+    );
+}
+
+const Welcome = () => {
+    return (
+        <h1>Hello, samurai! Let's go! Everything will be FINE! START FLYING!</h1>
+    );
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <p className="important">Need to know:</p>
+            <ol>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+            </ol>
         </div>
     );
 }
