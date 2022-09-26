bcad4b0bdc92832787b053a80a5e15cc8afa0e0b
Pikadorius <karpukea@yandex.by>
2022-09-26 16:35:02 +0300

    change app tsx/css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go! Everything will be FINE!
+     Hello, samurai! Let's go! Everything will be FINE! START FLYING!
+        <ul>
+            <li>Easy</li>
+            <li>Step by step</li>
+        </ul>
     </div>
   );
 }
