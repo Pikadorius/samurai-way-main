24e2797716da10d74c66ce52d275043e3be62aa0
Pikadorius <karpukea@yandex.by>
2022-09-27 22:08:55 +0300

    added new components

--- src/index.tsx
+++ src/index.tsx
@@ -9,15 +9,3 @@ ReactDOM.render(
     <App />,
   document.getElementById('root')
 );
-
-
-let user = {
-    name: "Egor",
-    age: 30,
-    address: {
-        city: "Minsk",
-        country: "Belarus",
-    },
-}
-
-console.log(user.address.city);
