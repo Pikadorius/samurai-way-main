3c64d48fb475e6660f1c9ebbc5e88d30acd40119
Pikadorius <karpukea@yandex.by>
2022-09-30 09:10:15 +0300

    add props for Accordion and AccordionTitle

--- src/Components/Accordion.tsx
+++ src/Components/Accordion.tsx
@@ -26,7 +26,11 @@ const AccordionBody = () => {
     console.log("AccordionBody rendering")
     return (
         <div>
-            <p>I am AccordionBody</p>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
         </div>
     );
 }
