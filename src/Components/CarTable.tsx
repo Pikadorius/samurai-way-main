b7b814da1433dbc48d8ce5d3b0b137a9bb309e73
Pikadorius <karpukea@yandex.by>
2022-10-12 15:03:25 +0300

    change classNames

--- src/Components/CarTable.tsx
+++ src/Components/CarTable.tsx
@@ -7,8 +7,8 @@ type CarTablePropsType = {
 
 const CarTable = (props:CarTablePropsType) => {
     return (
-        <div>
-            <table className={"homework1"}>
+        <div className={'test'}>
+            <table>
                 {props.list.map((item,index)=><tr key={index+1}><th>{index+1}</th><td>{item.manufacturer}</td><td>{item.model}</td></tr>)}
             </table>
         </div>
