d33cdbce14a41f0661e2c55756f29f2718228e05
Pikadorius <karpukea@yandex.by>
2022-10-13 14:40:25 +0300

    added Buttons to Header.tsx

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,29 +1,28 @@
 import React, {MouseEvent} from "react";
+import Button from './Button';
 
 type HeaderPropsType = {
     title: string,
 }
 
 const Header = (props: HeaderPropsType) => {
-    console.log("Header rendering")
+    console.log("Header rendering");
 
-    // const myFirstSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
-    //     console.log("You subscribed to my news!");
-    // }
-    //
-    // const mySecondSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
-    //     console.log("Now and you!")
-    // }
 
-    const onClickHandler=(name:string)=>{
-        console.log(name);
+    const Button1Foo = (value: string, age: number) => {
+        console.log(value,age);
     }
+
+    const Button2Foo = (value: string) => {
+        console.log(value);
+    }
+
 
     return (
         <div>
-            <h2>{props.title}</h2>
-            <button onClick={(event)=>onClickHandler("Egor")}>News</button>
-            <button onClick={(event)=>onClickHandler("Petya")}>Subscribe</button>
+            <h1>{props.title}</h1>
+            <Button name={"News"} callBack={() => Button1Foo("i am button 1",21)}/>
+            <Button name={"About me"} callBack={() => Button2Foo("i am button 2")}/>
         </div>
     );
 }
