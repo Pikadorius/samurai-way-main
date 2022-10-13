1541615d35b2f6f35e03328eed969ad7e7e67b2c
Pikadorius <karpukea@yandex.by>
2022-10-13 16:05:17 +0300

    added StateExample.tsx for lesson about useState hook

---
+++ src/Components/StateExample.tsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+const StateExample = () => {
+    console.log("StateExample rendering")
+
+    let [a, setA] = useState(0);
+
+    const OnClickHandler = () => {
+        setA(++a);
+    }
+    const ResetButton = () => {
+        setA(0);
+    }
+
+
+    return (
+        <div>
+            <h1>{a}</h1>
+            <button onClick={OnClickHandler}>Number of clicks</button>
+            <button onClick={ResetButton}>Reset</button>
+        </div>
+    );
+};
+
+export default StateExample;
