afcd42601b5fa6f3a1eb62cfc7978ffe62f12d6e
Pikadorius <karpukea@yandex.by>
2022-10-22 14:54:07 +0300

    added Button component

--- src/Components/StateExample.tsx
+++ src/Components/StateExample.tsx
@@ -1,23 +1,29 @@
 import React, {useState} from 'react';
+import Button from './Button';
 
 const StateExample = () => {
     console.log("StateExample rendering")
 
     let [a, setA] = useState(0);
 
-    const OnClickHandler = () => {
+    const IncreaseValue = () => {
         setA(++a);
+    }
+    const DecreaseValue = () => {
+        setA(--a);
     }
     const ResetButton = () => {
         setA(0);
     }
 
-
     return (
         <div className={'BorderedComponent'}>
             <h1>{a}</h1>
-            <button onClick={OnClickHandler}>Number of clicks</button>
-            <button onClick={ResetButton}>Reset</button>
+            {/*<button onClick={IncreaseValue}>Number of clicks</button>
+            <button onClick={ResetButton}>Reset</button>*/}
+            <Button name={'Increase'} callBack={IncreaseValue}/>
+            <Button name={'Decrease'} callBack={DecreaseValue}/>
+            <Button name={'Reset'} callBack={ResetButton}/>
         </div>
     );
 };
