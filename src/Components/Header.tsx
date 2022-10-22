50e8416624427d8d28468faf62b3fc56bacc3b0f
Pikadorius <karpukea@yandex.by>
2022-10-22 14:13:27 +0300

    change place for pure functions

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,27 +1,25 @@
-import React, {MouseEvent} from "react";
+import React from "react";
 import Button from './Button';
 
 type HeaderPropsType = {
     title: string,
 }
 
+const Button1Foo = (value: string, numb: number) => {
+    console.log(value,numb);
+}
+
+const Button2Foo = () => {
+    console.log("i am stupid button");
+}
+
+
 const Header = ({title}:HeaderPropsType) => {
     console.log("Header rendering");
-
-
-    const Button1Foo = (value: string, age: number) => {
-        console.log(value,age);
-    }
-
-    const Button2Foo = () => {
-        console.log("i am stupid button");
-    }
-
-
     return (
         <div>
             <h1>{title}</h1>
-            <Button name={"News"} callBack={() => Button1Foo("i am button 1",21)}/>
+            <Button name={"News"} callBack={() => Button1Foo("i am button",21)}/>
             <Button name={"Stupid Button"} callBack={Button2Foo}/>
         </div>
     );
