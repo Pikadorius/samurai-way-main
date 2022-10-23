d2113174213a1dcaab38eeeafe5715262054a557
Pikadorius <karpukea@yandex.by>
2022-10-23 15:53:12 +0300

    component and subcomponents for input with button

--- src/Components/InputTests/FullInput.tsx
+++ src/Components/InputTests/FullInput.tsx
@@ -1,17 +1,26 @@
 import React, {ChangeEvent, useState} from 'react';
 
+type FullInputType= {
+    addMessage: (title:string)=>void
+}
 
-const FullInput = () => {
+const FullInput = (props:FullInputType) => {
     let [title, setTitle] = useState('')
     console.log('this is title -', title);
 
     const onChangeInputHandler = (event:ChangeEvent<HTMLInputElement>)=> {
         setTitle(event.currentTarget.value);
     }
+
+    const onClickButtonHandler=()=>{
+        props.addMessage(title);
+        setTitle("")
+    }
+
     return (
         <div>
-            <input onChange={onChangeInputHandler}/>
-            <button onClick={()=>{}}>+</button>
+            <input value={title} onChange={onChangeInputHandler}/>
+            <button onClick={onClickButtonHandler}>+</button>
         </div>
     );
 }
--- src/Components/NewComponentForInputs.tsx
+++ src/Components/NewComponentForInputs.tsx
@@ -1,8 +1,12 @@
 import React, {useState} from 'react';
 import FullInput from './InputTests/FullInput';
 
+type MessageType = {
+    message: string;
+}
+
 const NewComponentForInputs = () => {
-    const [message, setMessage] = useState([
+    const [message, setMessage] = useState<Array<MessageType>>([
             {message: 'message1'},
             {message: 'message2'},
             {message: 'message3'},
@@ -10,9 +14,14 @@ const NewComponentForInputs = () => {
             {message: 'message5'}
         ]
     )
+    const addMessage = (title:string) => {
+        let newMessage={message: title}
+        setMessage([newMessage,...message])
+    }
+
     return (
         <div>
-            <FullInput/>
+            <FullInput addMessage={addMessage}/>
             {message.map((el, index) => {
                 return (
                     <div key={index}>{el.message}</div>
