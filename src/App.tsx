fc7acf9b530a0b9f8553e17b2c17de26502f5677
Pikadorius <karpukea@yandex.by>
2022-10-23 15:54:05 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import Header from "./Components/Header";
-import Welcome from "./Components/Welcome";
-import Footer from "./Components/Footer";
 import Accordion from "./Components/Accordion";
 import Rating from "./Components/Rating";
-import CarTable from "./Components/CarTable";
 import NewComponent from "./Components/NewComponent";
 import StateExample from './Components/StateExample';
 import Banknotes from './Components/Banknotes';
-import Button from './Components/Button';
-import FullInput from './Components/InputTests/FullInput';
 import NewComponentForInputs from './Components/NewComponentForInputs';
-
-
-export type TopCarPropsType = {
-    manufacturer: string
-    model: string
-};
-
-const topCars: Array<TopCarPropsType> = [
-    {manufacturer: 'BMW', model: 'm5cs'},
-    {manufacturer: 'Mercedes', model: 'e63s'},
-    {manufacturer: 'Audi', model: 'rs6'}
-];
+import NewComponentForInputs2 from './Components/NewComponentForInputs2';
 
 export type StudentsPropsType = {
     id: number,
@@ -45,26 +28,15 @@ const students: Array<StudentsPropsType> = [
     {id: 11, name: "Christopher", age: 100},
 ];
 
-
-const acceptFoo = () => {
-    console.log('You accepted all sudjestions')
-}
-const dislineFoo = () => {
-    console.log('You dislined all sudjestions')
-}
-
-
 function App() {
     console.log("App rendering");
 
     return (
         <div className="App">
-            <Header title={"Choose your destiny!"}/>
             <NewComponentForInputs/>
+            <NewComponentForInputs2/>
             <Banknotes/>
             <StateExample/>
-            <CarTable cars={topCars}/>
-            <Welcome title={"Wake the fuck up, Samurai! We have a city to BURN!"}/>
             <Accordion titleValue={"You CAN do it, just follow these directions:"}
                        listValue={["Stand your ground", "Step by step", "Never give UP", "LOL"]}
                        collapsed={false}/>
@@ -72,19 +44,8 @@ function App() {
             <Accordion titleValue={"Need to know:"}
                        listValue={["HTML", "CSS", "JS"]}
                        collapsed={false}/>
-            <Rating value={4} color={"red"}/>
-            <Accordion titleValue={"Menu"}
-                       listValue={["Porridge", "Pizza", "Meat"]}
-                       collapsed={false}/>
-            <Rating value={4} color={"red"}/>
-            <Accordion titleValue={"Menu 2"}
-                       listValue={["Tea", "Coffee"]}
-                       collapsed={true}/>
             <Rating value={5} color={"red"}/>
             <NewComponent students={students}/>
-            <Button name={"Accept all"} callBack={acceptFoo}/>
-            <Button name={"Disline"} callBack={dislineFoo}/>
-            <Footer/>
         </div>
     );
 }
