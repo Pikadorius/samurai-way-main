1865bc28f601e66468f532706038aa568a05cd7b
Pikadorius <karpukea@yandex.by>
2022-11-01 22:19:13 +0300

    fix id bug (added v1())

--- src/Components/ToDoList/TodoList.tsx
+++ src/Components/ToDoList/TodoList.tsx
@@ -2,9 +2,10 @@ import React, {useState} from 'react';
 import Input from '../InputTests/Input';
 import Button from '../Button';
 import s from './ToDoList.module.css'
+import {v1} from 'uuid';
 
-type AffairType={
-    id: number
+type AffairType = {
+    id: string
     name: string
 }
 
@@ -13,20 +14,20 @@ type AffairsType = AffairType[]
 const TodoList = () => {
 
     const [affairs, setAffairs] = useState<AffairsType>([
-        {id:1, name: 'Learn about INPUT'},
-        {id:2, name: 'Learn about callbacks'},
-        {id:3, name: 'Write todolist'},
+        {id: v1(), name: 'Learn about INPUT'},
+        {id: v1(), name: 'Learn about callbacks'},
+        {id: v1(), name: 'Write todolist'},
     ]);
-    const [title, setTitle]=useState('');
+    const [title, setTitle] = useState('');
 
     const addAffair = () => {
-        let newAffair={id:affairs[affairs.length-1].id+1, name:title}
+        let newAffair = {id: v1(), name: title}
         setAffairs([...affairs, newAffair])
         setTitle("")
     }
 
-    const deleteAffairs = (id:number) => {
-        setAffairs(affairs.filter(i=>i.id!==id));
+    const deleteAffairs = (id: string) => {
+        setAffairs(affairs.filter(i => i.id !== id));
     }
 
 
@@ -34,12 +35,12 @@ const TodoList = () => {
         <div className={s.wrapper}>
             <h2>Need to do:</h2>
             <ol>
-                {affairs.map((affair)=>{
+                {affairs.map((affair) => {
                     return (
                         <li key={affair.id}>
                             {affair.name}
-                           {/* <button onClick={(event)=>deleteAffairs(affair.id)}>-</button>*/}
-                            <Button name={'-'} callBack={()=>deleteAffairs(affair.id)}/>
+                            {/* <button onClick={(event)=>deleteAffairs(affair.id)}>-</button>*/}
+                            <Button name={'-'} callBack={() => deleteAffairs(affair.id)}/>
                         </li>
                     )
                 })}
