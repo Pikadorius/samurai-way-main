55cbff57a77eedda0f31652118c8475ee9ad8a26
Pikadorius <karpukea@yandex.by>
2022-11-01 15:34:01 +0300

    added type for rating value

--- src/Components/Rating/UncontrolledRating.tsx
+++ src/Components/Rating/UncontrolledRating.tsx
@@ -1,37 +1,44 @@
-import React, {useState} from "react";
+import React, {useState, MouseEvent} from "react";
 import s from './Rating.module.css';
 import rating from './full.png'
 import norating from './empty.png'
 
 
+type RatingType = 1 | 2 | 3 | 4 | 5;
+
 type UncontolledRatingPropsType = {
-    rating: number
+    rating: RatingType
 }
 
 const UncontrolledRating: React.FC<UncontolledRatingPropsType> = ({rating}) => {
     console.log("Rating rendering")
-    const [value, setValue] = useState<number>(rating);
+    const [value, setValue] = useState<RatingType>(rating);
+    const setRating = (rating: RatingType) => {
+        return (
+            () => setValue(rating)
+        )
+    }
 
     return (
         <div className={s.rating}>
-            <Star selected={value > 0} callBack={() => setValue(1)}/>
-            <Star selected={value > 1} callBack={() => setValue(2)}/>
-            <Star selected={value > 2} callBack={() => setValue(3)}/>
-            <Star selected={value > 3} callBack={() => setValue(4)}/>
-            <Star selected={value > 4} callBack={() => setValue(5)}/>
+            <Star selected={value > 0} setRating={setRating(1)}/>
+            <Star selected={value > 1} setRating={setRating(2)}/>
+            <Star selected={value > 2} setRating={setRating(3)}/>
+            <Star selected={value > 3} setRating={setRating(4)}/>
+            <Star selected={value > 4} setRating={setRating(5)}/>
         </div>
     );
 }
 
 type StarPropsType = {
     selected: boolean,
-    callBack: () => void
+    setRating: (e: MouseEvent<HTMLInputElement>) => void
 }
 
-const Star = ({selected, callBack}: StarPropsType) => {
+const Star = ({selected, setRating}: StarPropsType) => {
     console.log("Star rendering")
     return (
-        <span onClick={callBack}>
+        <span onClick={setRating}>
             {selected ? <img className={s.star} alt={'yes'} src={rating}/> :
                 <img className={s.star} alt={'no'} src={norating}/>}
         </span>
