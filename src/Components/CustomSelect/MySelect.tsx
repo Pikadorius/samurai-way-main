0b3a5c4021f88b9065bf2b0bdf89e5cd19def848
Pikadorius <karpukea@yandex.by>
2022-11-22 16:19:30 +0300

    added default select arrows behaviour

--- src/Components/CustomSelect/MySelect.tsx
+++ src/Components/CustomSelect/MySelect.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './MySelect.module.css'
 import arrowBottom from './ArrowBottom.png'
 import arrowTop from './ArrowTop.png'
@@ -17,10 +17,14 @@ type SelectPropsType = {
 
 const MySelect = (props: SelectPropsType) => {
     const [collapsed, setCollapsed] = useState<boolean>(true)
-    const [hoveredElement, setHoveredElement]=useState(props.value)
+    const [hoveredElement, setHoveredElement] = useState(props.value)
 
-    const item=props.itemsList.find(i=>i.value===props.value)
-    const hoveredItem=props.itemsList.find(i=>i.value===hoveredElement)
+    const item = props.itemsList.find(i => i.value === props.value)
+    const hoveredItem = props.itemsList.find(i => i.value === hoveredElement)
+
+    useEffect(() => {
+        setHoveredElement(props.value)
+    }, [props.value])
 
     const onTitleClickHandler = () => {
         setCollapsed(!collapsed)
@@ -31,18 +35,33 @@ const MySelect = (props: SelectPropsType) => {
         onTitleClickHandler();
     }
 
+    const onKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        console.log(e.key)
+        for (let i = 0; i < props.itemsList.length; i++) {
+            if (props.itemsList[i].value === hoveredElement) {
+                const pretender=e.key==='ArrowDown'? props.itemsList[i+1]:e.key==='ArrowUp'? props.itemsList[i-1]:props.itemsList[i]
+                if(pretender) {
+                    props.onChange(pretender.value)
+                    break
+                }
+            }
+        }
+    }
+
+
     return (
-        <div className={s.wrapper}>
+        <div className={s.wrapper} tabIndex={0} onKeyDown={onKeyPress}>
             <div className={s.selectName}>{item && item.title}
-                <div  onClick={onTitleClickHandler} className={s.arrow}> {collapsed ? <img alt={'arrowBottom'} src={arrowBottom}/> :
+                <div onClick={onTitleClickHandler} className={s.arrow}> {collapsed ?
+                    <img alt={'arrowBottom'} src={arrowBottom}/> :
                     <img alt={'arrowTop'} src={arrowTop}/>}</div>
             </div>
             {!collapsed && <div className={s.valuesField}>
                 {props.itemsList.map(i => <div
-                    onMouseEnter={()=>setHoveredElement(i.value)}
+                    onMouseEnter={() => setHoveredElement(i.value)}
                     key={i.value}
-                    className={s.selector + ' ' + (hoveredItem===i && s.selected)}
-                    onClick={()=>onItemClickHandler(i.value)}> {i.title}</div>)}
+                    className={s.selector + ' ' + (hoveredItem === i && s.selected)}
+                    onClick={() => onItemClickHandler(i.value)}> {i.title}</div>)}
             </div>}
         </div>
     );
