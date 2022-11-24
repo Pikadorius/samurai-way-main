b7fbe3f539b8ee7d27d8fc334556c6fc0511ecd9
Pikadorius <karpukea@yandex.by>
2022-11-24 16:38:09 +0300

    add any to action because need to test throw error

--- src/reducers/accordionReducer.ts
+++ src/reducers/accordionReducer.ts
@@ -1,19 +1,20 @@
-export const accordionReducer = (state:boolean, action:ActionType)=>{
+import {StateType} from '../Components/Accordion/UncontrolledAccordion';
+
+export const accordionReducer = (state: StateType, action: ActionType | any) => {
     switch (action.type) {
         case 'TOGGLE_COLLAPSED': {
-            return action.payload.collapsed;
+            return {...state, collapsed: action.payload}
         }
-        default: return state
+        default:
+            throw new Error('Bad action type')
     }
 }
 
 
-type ActionType=ReturnType<typeof collapseAccordionAC>
-export const collapseAccordionAC=(collapsed: boolean)=>{
+type ActionType = ReturnType<typeof collapseAccordionAC>
+export const collapseAccordionAC = (collapsed: boolean) => {
     return {
         type: "TOGGLE_COLLAPSED",
-        payload: {
-            collapsed
-        }
+        payload: collapsed
     } as const
 }
