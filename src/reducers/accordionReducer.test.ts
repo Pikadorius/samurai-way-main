aa2b00fd026e5602159b615bafa8532ce83aa3c6
Pikadorius <karpukea@yandex.by>
2022-11-24 16:38:28 +0300

    writed unit test for accordion reducer

---
+++ src/reducers/accordionReducer.test.ts
@@ -0,0 +1,20 @@
+import {accordionReducer, collapseAccordionAC} from './accordionReducer';
+import {StateType} from '../Components/Accordion/UncontrolledAccordion';
+
+let state:StateType;
+beforeEach(()=>{
+    state={
+        collapsed: false
+    }
+})
+
+
+test('reducer should change value to true', ()=>{
+    const newState = accordionReducer(state, collapseAccordionAC(!state.collapsed))
+
+    expect(newState.collapsed).toBe(true)
+})
+
+test('reducer should throw error because action type is incorrect', ()=>{
+    expect(()=>{accordionReducer(state, {type: 'FAKE_TYPE'})}).toThrowError()
+})
